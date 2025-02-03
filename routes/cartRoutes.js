3e856112b421ac8c0d31c25a0bd215167b9b783e
watchlab-co <officialwatchlab@gmail.com>
2025-02-03 15:06:12 +0530

    update:cors error cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,7 +5,7 @@ import authUser from '../middleware/auth.js'
 
 const cartRouter = express.Router()
 
-cartRouter.post('/get', authUser, getUserCart)
+// cartRouter.post('/get', authUser, getUserCart)
 cartRouter.post('/add', authUser, addToCart)
 cartRouter.post('/update', authUser, updateCart)
 
