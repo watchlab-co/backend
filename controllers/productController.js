10487ac42cd6cbbf309af67cc870e65066e94537
shaamiilll <shamilamiyan@gmail.com>
2025-04-07 14:15:47 +0530

    Updated the backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,24 +5,39 @@ import productModel from '../models/productModel.js';
 const addProduct = async (req, res) => {
     try {
         const { name, description, price, discount, category, subCategory, colours, stock, dialColor, strapMaterial, features, movement, bestseller } = req.body;
-        
-        
+
+
+
         // Get images from request
         const image1 = req.files.image1 && req.files.image1[0];
         const image2 = req.files.image2 && req.files.image2[0];
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
+        const video = req.files.video && req.files.video?.[0];
 
         // Filter out undefined images
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
 
-        // Upload images to cloudinary
-        let imagesUrl = await Promise.all(
+         // Upload images to cloudinary
+         let imagesUrl = await Promise.all(
             images.map(async (item) => {
                 let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
                 return result.secure_url;
             })
         );
+
+         // Filter out undefined images
+         const videos = [video].filter((item) => item !== undefined);
+
+         // Upload images to cloudinary
+         let videoUrl = await Promise.all(
+            videos.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'video' });
+                return result.secure_url;
+            })
+        );
+    
+        
         const parsedStock = stock === "Yes" ? true : false;
 
         const AdminId = req.user.id; // Assuming user authentication middleware adds `req.user`
@@ -41,6 +56,7 @@ const addProduct = async (req, res) => {
             movement,
             bestseller: bestseller === "true" ? true : false,
             image: imagesUrl,
+            video: videoUrl,
             date: Date.now(),
             shopId: AdminId
         };
@@ -115,7 +131,7 @@ const UpdateProduct = async (req, res) => {
         const { productId } = req.params;
         const updateData = req.body;
 
-        
+
 
         const updatedProduct = await productModel.findByIdAndUpdate(productId, updateData, {
             new: true,
